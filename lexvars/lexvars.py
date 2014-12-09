77abb7a114ef2b77207e8134cc44b0a220792077
awjamison <awj228@nyu.edu>
2014-12-09 15:33:34 -0500

    Changes LexVars class to operate on a csv table of words rather than individual words. Initialized with the file path. Method calls update the table with new columns/variables. Additional method attributes added.

--- lexvars/lexvars.py
+++ lexvars/lexvars.py
@@ -3,13 +3,24 @@
 # License: BSD (3-clause)
 
 import __builtin__
+import csv
 import collections
 import itertools
 import os
 
 import numpy as np
 
+from nltk.corpus import wordnet as wn
 from celex import Celex
+
+if 'clx' not in globals():
+    path_to_clx = os.path.expanduser("~/Dropbox/corpora/CELEX_V2/english")
+    clx = Celex(path_to_clx)
+    clx.load_lemmas()
+    clx.load_wordforms()
+    clx.map_lemmas_to_wordforms()
+
+path_to_ds = os.path.expanduser("~/Dropbox/SufAmb/all_subjects.csv")
 
 
 class LexVars(object):
@@ -21,18 +32,41 @@ class LexVars(object):
     lv.inflectional_entropy('shoe')
     '''
 
-    def __init__(self, clx):
-        self.clx = clx
+    def __init__(self, path=path_to_ds):
+        self.load(path)
 
-    def wordnet_synsets(self, word):
+    def load(self, path):
+        self.data = list(csv.DictReader(open(path)))
+        self.words = set(rec['Word'] for rec in self.data)
+
+    def wordnet_synsets(self):
         '''
         Number of WordNet "synsets" (roughly, senses) for the given word.
         This variable collapses across different parts of speech, meanings,
         etc.
         '''
-        from nltk.corpus import wordnet
-        synsets = wordnet.synsets(word)
-        return len(synsets) if len(synsets) > 0 else None
+        for record in self.data:
+            synsets = wn.synsets(record['Word'])
+            if len(synsets) > 0:
+                record['n_synsets'] = len(synsets)
+            else:
+                record['n_synsets'] = 0
+
+    def synset_ratio(self):
+
+        for record in self.data:
+            n_synsets = len(wn.synsets(record['Word'], 'n'))
+            v_synsets = len(wn.synsets(record['Word'], 'v'))
+            if n_synsets > 0:
+                record['vtn_synsets'] = v_synsets / float(n_synsets)
+            else:
+                record['vtn_synsets'] = 0
+
+    def synset_entropy(self, word):
+        raise NotImplementedError('Not implemented.')
+
+    def synset_entropy_ratio(self, word):
+        raise NotImplementedError('Not implemented.')
 
     def pos_freq(self, lemma, pos):
         '''
@@ -41,19 +75,20 @@ class LexVars(object):
         >>> lv.pos_freq('wind', 'noun')
         3089
         '''
-        lemmas = self.clx.lemma_lookup(lemma)
+        lemmas = clx.lemma_lookup(lemma)
         return sum(lemma.Cob for lemma in lemmas if lemma.ClassNum == pos)
 
     def _smoothed_log_ratio(self, a, b):
         return np.log2((a + 1.) / (b + 1.))
 
     def log_noun_to_verb_ratio(self, lemma):
-        noun_freq = self.pos_freq(lemma, 'noun')
-        verb_freq = self.pos_freq(lemma, 'verb')
-        return _smoothed_log_ratio(noun_freq, verb_freq)
+        for record in self.data:
+            noun_freq = self.pos_freq(record['Word'], 'noun')
+            verb_freq = self.pos_freq(record['Word'], 'verb')
+            ratio = _smoothed_log_ratio(noun_freq, verb_freq)
+            record['log_ntv_ratio'] = ratio
 
-    def inflectional_entropy(self, lemma, kind='separate_bare', smooth=1,
-                             verbose=False):
+    def inflectional_entropy(self, smooth=1, verbose=False):
         '''
         This function collapses across all relevant lemmas, e.g. the noun 
         "build" and the verb "build", or the various "wind" verbs.
@@ -96,57 +131,91 @@ class LexVars(object):
         aching (participle), ached (past tense), ached (participles)
         '''
 
-        clx_lemmas = self.clx.lemma_lookup(lemma)
-        # Use __builtin__ here in case sum is overshadowed by numpy
-        all_wordforms = __builtin__.sum((self.clx.lemma_to_wordforms(clx_lemma) 
-                                         for clx_lemma in clx_lemmas), [])
+        for record in self.data:
+            clx_lemmas = clx.lemma_lookup(record['Word'])
+            # Use __builtin__ here in case sum is overshadowed by numpy
+            all_wordforms = __builtin__.sum((clx.lemma_to_wordforms(clx_lemma)
+                                             for clx_lemma in clx_lemmas), [])
 
-        counter = collections.Counter()
+            counter = collections.Counter()
 
-        for wf in all_wordforms:
-            infl = wf.FlectType
-            freq = wf.Cob
-            if (infl[0] == 'present_tense' and infl[1] != '3rd_person_verb' 
-                or infl[0] == 'infinitive'):
-                counter['bare_verb'] += freq
-            if infl[0] == 'singular':
-                counter['bare_noun'] += freq
-            if infl[0] == 'plural':
-                counter['noun_plural'] += freq
-            if infl[0] == 'past_tense':
-                counter['past_tense'] += freq
-            if infl == ['positive']:
-                counter['positive'] += freq
-            if infl == ['comparative']:
-                counter['comparative'] += freq
-            if infl == ['superlative']:
-                counter['superlative'] += freq
-            if infl == ['headword_form']:
-                counter['headword_form'] += freq
-            if infl == ['present_tense', '3rd_person_verb', 'singular']:
-                counter['third_sg'] += freq
-            if infl == ['participle', 'present_tense']:
-                counter['part_ing'] += freq
-            if infl == ['participle', 'past_tense']:
-                counter['part_ed'] += freq
+            for wf in all_wordforms:
+                infl = wf.FlectType
+                freq = wf.Cob
+                if (infl[0] == 'present_tense' and infl[1] != '3rd_person_verb'
+                    or infl[0] == 'infinitive'):
+                    counter['bare_verb'] += freq
+                if infl[0] == 'singular':
+                    counter['bare_noun'] += freq
+                if infl[0] == 'plural':
+                    counter['noun_plural'] += freq
+                if infl[0] == 'past_tense':
+                    counter['past_tense'] += freq
+                if infl == ['positive']:
+                    counter['positive'] += freq
+                if infl == ['comparative']:
+                    counter['comparative'] += freq
+                if infl == ['superlative']:
+                    counter['superlative'] += freq
+                if infl == ['headword_form']:
+                    counter['headword_form'] += freq
+                if infl == ['present_tense', '3rd_person_verb', 'singular']:
+                    counter['third_sg'] += freq
+                if infl == ['participle', 'present_tense']:
+                    counter['part_ing'] += freq
+                if infl == ['participle', 'past_tense']:
+                    counter['part_ed'] += freq
 
-        common = ['noun_plural', 'third_sg', 'part_ing', 'part_ed', 
-                  'past_tense', 'comparative', 'superlative']
-        bare = ['bare_noun', 'bare_verb', 'positive', 'headword_form']
-        common_freqs = [counter[i] for i in common if i in counter]
-        bare_freqs = [counter[i] for i in bare if i in counter]
+            common = ['noun_plural', 'third_sg', 'part_ing', 'part_ed',
+                      'past_tense', 'comparative', 'superlative']
+            bare = ['bare_noun', 'bare_verb', 'positive', 'headword_form']
+            common_freqs = [counter[i] for i in common if i in counter]
+            bare_freqs = [counter[i] for i in bare if i in counter]
 
-        if verbose:
-            print counter
+            if verbose:
+                print counter
 
-        if kind == 'separate_bare':
-            return self.entropy(bare_freqs + common_freqs, smooth)
-        elif kind == 'collapsed_bare':
-            return self.entropy([sum(bare_freqs)] + common_freqs, smooth)
-        elif kind == 'no_bare':
-            return self.entropy(common_freqs, smooth)
-        else:
-            raise ValueError('Unknown inflectional entropy kind "%s"' % kind)
+            record['infl_ent_separate_bare'] = self.entropy(bare_freqs + common_freqs, smooth)
+            record['infl_ent_collapsed_bare'] = self.entropy([sum(bare_freqs)] + common_freqs, smooth)
+            record['infl_ent_no_bare'] = self.entropy(common_freqs, smooth)
+
+    def derivational_family_size(self):
+
+        by_morpheme = {}
+
+        for lemma in clx._lemmas:
+            if len(lemma['Parses']) < 1:
+                continue
+            decomposition = lemma['Parses'][0]['Imm']
+            morphemes = decomposition.split('+')
+            for morpheme in morphemes:
+                if morpheme in self.words:
+                    by_morpheme.setdefault(morpheme, set()).add(
+                            lemma['Head'])
+
+        for record in self.data:
+            decompositions = by_morpheme.get(record['Word'])
+            if decompositions is not None:
+                record['derivational_family_size'] = len(decompositions)
+            else:
+                record['derivational_family_size'] = None
+
+    def derivational_family_entropy(self):
+
+        by_morpheme = {}
+
+        for lemma in clx._lemmas:
+            if len(lemma['Parses']) < 1:
+                continue
+            decomposition = lemma['Parses'][0]['Imm']
+            morphemes = decomposition.split('+')
+            if morphemes[0] in self.words:
+                by_morpheme.setdefault(morphemes[0], list()).append(lemma)
+
+        for record in self.data:
+            derived = by_morpheme.get(record['Word'])
+            freqs = [x['Cob'] for x in derived]
+            record['derivational_entropy'] = self.entropy(freqs)
 
     def entropy(self, freq_vec, smoothing_constant=1):
         '''
@@ -164,14 +233,3 @@ class LexVars(object):
         # then p(x) * log(p(x)) = 0 in the definition of entropy)
         probs[probs == 0] = 1
         return -np.sum(probs * np.log2(probs))
-
-
-def test_all():
-    clx = Celex(os.path.expanduser('~/Dropbox/celex_english'))
-    clx.load_lemmas()
-    clx.load_wordforms()
-    clx.map_lemmas_to_wordforms()
-    lv = LexVars(clx)
-    unique_lemmas = set(x['Head'] for x in clx._lemmas)
-    for lemma in unique_lemmas:
-        lv.inflectional_entropy(lemma)
